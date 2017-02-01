c4da3cb34dfcf825a90d4c46537bf7bd3d75dccf
Marcos <marcosjbarroso82@gmail.com>
2017-02-01 12:15:08 -0300

    locket url rows when incrementing counters

--- apps/url/views.py
+++ apps/url/views.py
@@ -2,16 +2,18 @@ from django.http.response import HttpResponseRedirect
 from django.views.generic.detail import BaseDetailView
 from .models import DeviceUrl, ShortUrl
 from user_agents import parse
+from django.db.transaction import atomic
 
 
 class ShortUrlDetailView(BaseDetailView):
     model = ShortUrl
 
+    @atomic
     def get(self, request, *args, **kwargs):
         device = None
 
         user_agent = parse(request.environ.get('HTTP_USER_AGENT'))
-        obj = self.get_object()
+        obj = self.get_object(queryset=ShortUrl.objects.select_for_update().all())
         redirect_url = obj.target
 
         if user_agent.is_mobile:
@@ -22,10 +24,11 @@ class ShortUrlDetailView(BaseDetailView):
             device = 'desktop'
 
         if device:
-            device_url = obj.get_device_url(device)
-            device_url.counter += 1
-            device_url.save()
-            redirect_url = device_url.target
+            device_url = obj.device_urls.select_for_update().filter(type=device).first()
+            if device_url:
+                device_url.counter += 1
+                device_url.save()
+                redirect_url = device_url.target
 
         obj.counter += 1
         obj.save()
